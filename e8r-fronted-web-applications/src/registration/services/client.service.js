4f64ba5dd7b2205f8ed1547c2a862578e1ced826
AndreVMG <133740030+AndreVMG@users.noreply.github.com>
2025-09-19 17:05:01 -0500

    feat: Implement methods in client.service

--- e8r-fronted-web-applications/src/registration/services/client.service.js
+++ e8r-fronted-web-applications/src/registration/services/client.service.js
@@ -4,4 +4,28 @@ export class CustomerService extends BaseService {
     constructor(){
         super("customers");
     }
+
+    async getCustomerPaginated(page = 1, pageSize = 10) {
+        return this.get(`/paginated?page=${page}&pageSize=${pageSize}`);
+    }
+
+    async getCustomerById(customerId) {
+        return this.get(`/${customerId}`);
+    }
+
+    async createCustomer(customer) {
+        return this.post("", customer);
+    }
+
+    async getCustomerByName(name) {
+        return this.get(`/client-name/${encodeURIComponent(name)}`);
+    }
+
+    async getCustomerByDni(dni) {
+        return this.get(`/client-dni/${encodeURIComponent(dni)}`);
+    }
+
+    async getCustomerByRuc(ruc) {
+        return this.get(`/client-ruc/${encodeURIComponent(ruc)}`);
+    }
 }
--- e8r-fronted-web-applications/src/visualization/pages/home-client.component.vue
+++ e8r-fronted-web-applications/src/visualization/pages/home-client.component.vue
@@ -2,10 +2,26 @@
 import HeaderBar from "../../public/components/header-bar.component.vue";
 import TableClients from "../components/table-clients.component.vue";
 
+import {Customer} from "../../registration/models/client.entity.js";
+import {CustomerService} from "../../registration/services/client.service.js";
+
 export default{
+  name: "home-client",
   components: {
     HeaderBar,
     TableClients
+  },
+  computed: {
+    id(){
+      return this.$route.params.id;
+    }
+  },
+  data(){
+    return {
+      customerService: new CustomerService(),
+      customers: [],
+      customer: Customer,
+    }
   }
 }
 </script>
@@ -17,6 +33,6 @@ export default{
 
 <style scoped>
 .table-clients {
-  margin-top: 5rem;
+  margin-top: 13rem;
 }
 </style>
